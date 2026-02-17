38d1ac8a42773fa45c58023b49ef1a69283463d8
AjayMI <ajayabburi22@gmail.com>
2026-02-16 23:55:49 -0600

    L:125 - N:two pointers - Valid Palindrome

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -189,3 +189,20 @@ class Solution:
                 rows[i].add(board[i][j])
                 square[((i//3)*3+j//3)].add(board[i][j])
         return True
+    
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
+        return True
