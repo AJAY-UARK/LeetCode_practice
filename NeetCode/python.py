41582e67068e87338eeb9e9fa38706063f943cc5
AjayMI <ajayabburi22@gmail.com>
2026-02-18 22:16:10 -0600

    L:15 - N:Med two pinters - 3sum

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -217,3 +217,26 @@ class Solution:
             
             hashTable[difference] = i
         return []
+    
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        for i in range(len(nums)-1):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            j = i + 1
+            k = len(nums) -1
+            while j < k:
+                # target = -nums[i]
+                if -nums[i] < (nums[j] + nums[k]):
+                    k -= 1
+                elif -nums[i] > (nums[j] + nums[k]):
+                    j += 1
+                else:
+                    ans.append([nums[i],nums[j],nums[k]])
+                    j += 1
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
+                    k -= 1
+
+        return ans
