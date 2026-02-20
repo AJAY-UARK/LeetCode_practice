05321e28dbc3cf607184ee2e81342a92e63ed712
AjayMI <ajayabburi22@gmail.com>
2026-02-19 23:52:56 -0600

    L:11 - N:2pointers - Container with more water

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -240,3 +240,18 @@ class Solution:
                     k -= 1
 
         return ans
+    
+    def maxArea(self, heights: List[int]) -> int:
+        if len(heights) < 2:
+            return 0
+        l, r = 0, len(heights)-1
+        max_area = 0
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            if max_area < area:
+                max_area = area
+            if heights[l] > heights[r]:
+                r -= 1
+            else:
+                l += 1
+        return max_area
