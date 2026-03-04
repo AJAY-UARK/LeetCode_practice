06f3ddf1cf4d912cf0df5f1753356420d59be2a4
AjayMI <ajayabburi22@gmail.com>
2026-03-03 23:41:44 -0600

    L:853 - M:Med stack - Car fleet

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -393,6 +393,16 @@ class Solution:
             stack.append(i)
 
         return answer
+    
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [[p,s] for p, s in zip(position, speed)]
+
+        stack = []
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target-p)/s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
 
 class MinStack:
 
