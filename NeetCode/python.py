d98fc0a25428bfbe785eea48c8cfdd74d6be3ff5
AjayMI <ajayabburi22@gmail.com>
2026-03-04 23:33:08 -0600

    L:853 - M:Med stack - Car fleet - sort before loop

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -395,11 +395,19 @@ class Solution:
         return answer
     
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p,s] for p, s in zip(position, speed)]
+        # pair = [[p,s] for p, s in zip(position, speed)]
 
+        # stack = []
+        # for p, s in sorted(pair)[::-1]:
+        #     stack.append((target-p)/s)
+        #     if len(stack) >= 2 and stack[-1] <= stack[-2]:
+        #         stack.pop()
+        # return len(stack)
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
         stack = []
-        for p, s in sorted(pair)[::-1]:
-            stack.append((target-p)/s)
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
