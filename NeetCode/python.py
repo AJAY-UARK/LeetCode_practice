5223430fcf4ef97d97e75d28ed5c818602b69465
AjayMI <ajayabburi22@gmail.com>
2026-03-10 22:58:18 -0500

    L:424 - N:Med Sliding Window - Longest Repeating Character Replacement

--- NeetCode/python.py
+++ NeetCode/python.py
@@ -464,6 +464,21 @@ class Solution:
                     l = middle + 1
         return -1  
     
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        ans = 0
+        l = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+                
+            ans = max(ans, (r-l+1))
+            
+        return ans
 
 class MinStack:
 
